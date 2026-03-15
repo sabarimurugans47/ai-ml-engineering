5be2685ad9b9c05244731b25c67e9187fc40bc18
Sabari Murugan S <sabarimurugans2005@gmail.com>
2026-03-15 09:44:29 +0530

    Add files via upload

--- influenza_rna_similarity/src/load_data.py
+++ influenza_rna_similarity/src/load_data.py
@@ -1,1 +1,19 @@
+# src/load_data.py
 
+import pandas as pd
+from Bio import SeqIO
+
+def load_fasta_files(segment_files):
+    
+    records = []
+
+    for segment, file_path in segment_files.items():
+        for record in SeqIO.parse(file_path, "fasta"):
+            records.append({
+                "id": record.id,
+                "sequence": str(record.seq),
+                "length": len(record.seq),
+                "segment": segment
+            })
+
+    return pd.DataFrame(records)
