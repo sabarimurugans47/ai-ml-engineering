27629218b28afed7b55a7eef62e97b5f182408ac
Sabari Murugan S <sabarimurugans2005@gmail.com>
2026-03-15 09:44:54 +0530

    Add files via upload

--- influenza_rna_similarity/src/preprocess_data.py
+++ influenza_rna_similarity/src/preprocess_data.py
@@ -1,1 +1,15 @@
+from nltk.util import ngrams
 
+def dna_to_rna(seq):
+    return seq.replace("T", "U")
+
+def extract_kmers(seq, k=3):
+    return [''.join(g) for g in ngrams(seq, k)]
+
+def preprocess_dataframe(df, k=3):
+    
+    df = df.copy()
+    df['sequence'] = df['sequence'].apply(dna_to_rna)
+    df['kmers'] = df['sequence'].apply(lambda x: extract_kmers(x, k))
+
+    return df
